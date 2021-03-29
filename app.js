b0c22ae961c4f162791672e78b87970c80cc9e86
Shlomo Neuberger <shlomo.dev@neuberger.co.il>
2021-03-29 17:35:54 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,91 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const todoDB = require(__dirname + '/dbs/TodoDB');
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+
+
+
+
+app.get("/", function (req, res) {
+
+    todoDB.Item.find({}, function (err, foundItems) {
+
+        if (foundItems.length === 0) {
+            todoDB.Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully savevd default items to DB.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Today", newListItems: foundItems });
+        }
+    });
+
+});
+
+app.get("/:customListName", function (req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+
+    todoDB.List.findOne({ name: customListName }, function (err, foundList) {
+        if (!err) {
+            if (!foundList) {
+                //Create a new list
+                todoDB.newList(customListName)
+                res.redirect("/" + customListName);
+            } else {
+                //Show an existing list
+
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
+        }
+    });
+
+
+
+});
+
+app.post("/", function (req, res) {
+
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    todoDB.newItem(listName, itemName)
+
+    if (listName === "Today")
+        res.redirect("/");
+    else
+        res.redirect("/" + listName);
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    todoDB.deleteItemById(listName, checkedItemId)
+    if (listName == "Today") {
+        res.redirect("/");
+    }
+    else {
+        res.redirect("/" + listName);
+    }
+
+});
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
---
+++ dbs/TodoDb.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+exports.Item = Item
+exports.List = List
+
+exports.DeleteItemById = (id) => {
+
+}
+
+exports.newList = (customListName) => {
+    const list = new List({
+        name: customListName,
+        items: defaultItems
+    });
+    list.save();
+}
+
+exports.newItem = (listName, itemName) => {
+    const item = new Item({
+        name: itemName
+    });
+    if (listName === "Today") {
+        item.save();
+    } else {
+        List.findOne({ name: listName }, function (err, foundList) {
+            foundList.items.push(item);
+            foundList.save();
+        });
+    }
+}
+
+exports.deleteItemById = (listName, checkedItemId) => {
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function (err) { });
+    } else {
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) { });
+    }
+}
